c34c92fb454da1787fe277a1d852e33b2b8b5320
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-20 12:42:48 +0100

    update typist functionality and main page background

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import Typical from 'react-typical'
 
 const Hello = () => 
   {
     return (
-      <div className="ui main container">
-        <h1 id="hello">Hello World</h1>
+      <div className="main container">
+        <div className="background">
+          <div className="title">
+            <h1 id="hello">Hi, I'm Kayla Woobury</h1>
+            <p>I am 
+              <Typical
+                loop={Infinity}
+                wrapper='n'
+                steps={[
+                  ' an aspiring front end developer',
+                  2000,
+                  ' an aspiring back end developer',
+                  2000,
+                  ' a nature enthusiast',
+                  2000,
+                  ' a traveller',
+                  2000
+                ]}
+              />
+            </p>
+          </div>
+        </div>
       </div>
     );
   };
 
 export default Hello;
+
+
