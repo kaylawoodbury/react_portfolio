320de9db728fc677c03909276e876c8c28cb9c91
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-20 21:42:59 +0100

    updated header on projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -34,6 +34,7 @@ class Projects extends Component {
       <div className="ui main container"> 
       <div>     
         <h1 className="ui header"> My Projects </h1>
+        <h1 className="ui header"> Projects </h1><br/><br/>
         <div className="ui stackable four column grid">{projectsList}</div>
         </div> 
       </div>
