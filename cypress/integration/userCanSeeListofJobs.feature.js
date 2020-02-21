684947c126336e362f70cebef11f5b2503bbfc6c
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-21 15:11:52 +0100

    added descriptions to cv items

--- cypress/integration/userCanSeeListofJobs.feature.js
+++ cypress/integration/userCanSeeListofJobs.feature.js
@@ -8,7 +8,7 @@ describe('User can see list of cv items', () => {
     cy.get('#item-1').within(() => {
       cy.get(('.image')).should('exist');
       cy.get('.ui.header').should('contain', 'Craft Academy');
-      cy.get('.description').should('contain', '');
+      cy.get('.description').should('contain', 'Attended full stack development bootcamp.');
       cy.get('.link').should('exist');
     })
   });
@@ -17,7 +17,7 @@ describe('User can see list of cv items', () => {
     cy.get('#item-2').within(() => {
       cy.get(('.image')).should('exist');
       cy.get('.ui.header').should('contain', 'Intelex');
-      cy.get('.description').should('contain', '');
+      cy.get('.description').should('contain', 'Worked with SQL to import, organize and manipulate client data into a relational database.');
       cy.get('.link').should('exist');
     })
   });
@@ -26,7 +26,7 @@ describe('User can see list of cv items', () => {
     cy.get('#item-3').within(() => {
       cy.get(('.image')).should('exist');
       cy.get('.ui.header').should('contain', 'Ecocion');
-      cy.get('.description').should('contain', "");
+      cy.get('.description').should('contain', "Ecocion was acquired by Intelex, held the same job here as at Intelex.");
       cy.get('.link').should('exist');
     })
   });
@@ -35,7 +35,7 @@ describe('User can see list of cv items', () => {
     cy.get('#item-3').within(() => {
       cy.get(('.image')).should('exist');
       cy.get('.ui.header').should('contain', 'Dinosaur Ridge');
-      cy.get('.description').should('contain', "");
+      cy.get('.description').should('contain', "Worked as an educational tour guide and helped where needed to maintain the geologic gems of the ridge.");
       cy.get('.link').should('exist');
     })
   });
@@ -44,7 +44,7 @@ describe('User can see list of cv items', () => {
     cy.get('#item-3').within(() => {
       cy.get(('.image')).should('exist');
       cy.get('.ui.header').should('contain', 'Colorado School of Mines');
-      cy.get('.description').should('contain', "");
+      cy.get('.description').should('contain', "Graduated with a BS in Geological Engineering and a Minor in Public Affairs");
       cy.get('.link').should('exist');
     })
   });
